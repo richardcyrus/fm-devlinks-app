33fc1586818abb076bd9f27669907804a5953cbd
Richard Cyrus <richard.cyrus@rcyrus.com>
2023-12-01 19:06:05 -0500

    Add base styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 /// <reference types="vitest" />
 import { unstable_vitePlugin as remix } from '@remix-run/dev'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Unfonts from 'unplugin-fonts/vite'
 import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -27,6 +29,21 @@ export default defineConfig({
             { name: 'removeDimensions' },
           ],
         },
+      },
+    }),
+    Unfonts({
+      custom: {
+        families: [
+          {
+            name: 'Instrument Sans',
+            local: 'Instrument Sans',
+            src: '/public/fonts/InstrumentSans*.ttf',
+          },
+        ],
+        display: 'auto',
+        preload: true,
+        prefetch: false,
+        injectTo: 'head-prepend',
       },
     }),
   ],
